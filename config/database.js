d6017c18b23dee058fcd97c3ee251d95d04d08ac
Samuel Carvalho <carvalho.asamuel@gmail.com>
2022-12-06 14:21:32 -0300

    fixing database file

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'strapi_db'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'papel123'),
+      password: env('DATABASE_PASSWORD', 'Papel123@'),
       ssl: env.bool('DATABASE_SSL', ),
     },
     pool: {
